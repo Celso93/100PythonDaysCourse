95681e4b0c3e5b83355c71dc06926fb28f7419ba
Celso <celso.takashi07@gmail.com>
2025-03-23 18:10:30 -0300

    day2 completed

---
+++ 2/comments-day2.py
@@ -0,0 +1,53 @@
+# Data Types
+
+# Primite Types
+# subscripting: getting an element from index ?
+# Strings
+# subscripting: getting an element from index ? Staging by 0
+print("Hello!"[0])
+print("Hello!"[-1]) # last caracter
+
+
+# Integer: whole number without any decimal
+# large number 
+print(193_193_193) # 193,193,193
+
+# Float = floating point number
+# it "float" on  number
+# 3.1533
+
+# Boolean
+print(True)
+print(False)
+
+# sequence (string,bytes,tuple or range) x collection (dictonary, set, fronzer set)
+print( type("Hello") ) # check the data type
+print( type(123) )
+print( type(123_123.123) )
+print( type(True) )  
+
+# Type casting : changing the datatype of a variable
+# be careful on change datatype.
+print(int("123") + int("123"))
+
+# exercise1.py
+
+# OPERATIONAL
+# + - * / // **
+# / will always returning a float (implicit)
+# // will remove float, be careful
+
+# PEMDAS
+# Parenthreses, Exponents, Multiplication/Division, addition/substration
+# left to right
+
+# round()
+# up or down depends de decimal number
+# round(3.9) result is 4
+# round(3.2) result is 3
+# round(value, 2)
+
+# Assign
+# example += 1: means the preveus value plus 1
+
+# f-string: similar to interpolation in other languages
---
+++ 2/exercises.py
@@ -0,0 +1,16 @@
+# exercise1
+name = len(input("Enter your name: "))
+print("Number of lettes in your name: " + str(name))
+
+#exercise2
+print(3*3+3/3-3) # will return 7
+print(3*3+3/3-3) # how to make this calculation to outputs 3 ?
+print(3*(3+3)/3-3)
+
+#another example
+#bmi is equal to the person's weight divided by the person's height squared.
+height = 1.73 
+weight = 83
+bmi = weight/(height**2)
+
+print(bmi)
